ec3796cae69428510cf078a7c33b9f5e19027c24
Fabian Wunsch <fabian@uriah.heep.sax.de>
2019-11-10 00:45:05 +0100

    Erste Stufe der Verarbeitung

--- frontend/dynamic.js
+++ frontend/dynamic.js
@@ -6,8 +6,21 @@ var latlngs = [
 var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
 async function getJsonAsync(name) 
 {
-  let response = await fetch(`${name}`);
-  let data = await response.json()
-  return data;
-}getJsonAsync('/dummy.json')
-  .then(data => console.log(data)); 
+	let response = await fetch(`${name}`);
+	let data = await response.json()
+	return data;
+}
+getJsonAsync('/dummy.json')
+	.then(function(data) {
+	var output = [];
+	var tramlines = data.tramlines;
+	for (i = 0; i < tramlines.length; i++){
+		var tramline = tramlines[i];
+		var tramoutput = tramline;
+		for (j = 0; j < tramline.points.length; j++){
+			tramoutput.points[j] = [tramline.points[j].lat, tramline.points[j].long];
+		}
+		output.push(tramoutput);
+	}
+	return output;
+}).then(output => console.log(output)); 
