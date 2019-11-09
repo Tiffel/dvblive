8efe2f54d5adf2137f47857f6832323ea59f26fc
Christian Scheer <Christian.Scheer@t-systems.com>
2019-11-09 20:43:26 +0100

    Erste Anfrage gegen EFA

---
+++ backend/src/main/java/de/hackathondd/dvblive/adapter/DummyController.java
@@ -0,0 +1,22 @@
+package de.hackathondd.dvblive.adapter;
+
+import de.hackathondd.dvblive.application.VvoQueryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Ein Dummy Controller, um manuell abfragen zu triggern, die später regelmäßig automatisch laufen
+ */
+@RestController(value = "dummy")
+public class DummyController {
+    private VvoQueryService vvoQueryService;
+
+    public DummyController(VvoQueryService vvoQueryService) {
+        this.vvoQueryService = vvoQueryService;
+    }
+
+    @GetMapping(value = "/locationInformationRequest")
+    public String test() {
+        return vvoQueryService.locationInformationRequest();
+    }
+}
