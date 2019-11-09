7c962c445fbd41c2128674c76472ceccd7b18522
Christian Scheer <christian.scheer@t-systems.com>
2019-11-09 20:02:43 +0100

    spring initialzier initial commit

---
+++ backend/src/main/java/de/hackathondd/dvblive/DvbliveApplication.java
@@ -0,0 +1,13 @@
+package de.hackathondd.dvblive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvbliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvbliveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/hackathondd/dvblive/DvbliveApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hackathondd.dvblive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvbliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
