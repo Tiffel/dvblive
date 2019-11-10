eb9cd1bc5e58cb023a116a980a1bd913b1f42f08
Fabian Wunsch <fabian@uriah.heep.sax.de>
2019-11-10 03:21:22 +0100

    Neue Haltestellenart für frontend adaptiert

--- frontend/dynamic.js
+++ frontend/dynamic.js
@@ -5,19 +5,16 @@ async function getJsonAsync(name)
 	let data = await response.json()
 	return data;
 }
-getJsonAsync('/dummy12.json')
+getJsonAsync('/haltestellen.json')
 	.then(function(data) {
-	for (i = 0; i < data.length; i++){
-		var tramline = data[i];
-		for (j = 0; j < tramline.haltestellen.length; j++){
-			var aktuelle_haltestelle = tramline.haltestellen[j]
-            L.circle([aktuelle_haltestelle.longitude, aktuelle_haltestelle.latitude], {
-                color: 'blue',
-                fillColor: '#00c1ff',
-                fillOpacity: 0.2,
-                radius: 20
-			}).addTo(mymap);
-		}
+	for (j = 0; j < data.length; j++){
+		var aktuelle_haltestelle = data[j]
+        L.circle([aktuelle_haltestelle.longitude, aktuelle_haltestelle.latitude], {
+           color: 'blue',
+           fillColor: '#00c1ff',
+           fillOpacity: 0.2,
+           radius: 20
+		}).addTo(mymap);
 	}
 	return data;
 }).then(function(output) {
