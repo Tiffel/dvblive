ff5b1e4258c2ee81f9855ea6bb061165cebc93b4
Christian Scheer <Christian.Scheer@t-systems.com>
2019-11-10 11:18:34 +0100

    abschnitte je linie

--- backend/src/main/java/de/hackathondd/dvblive/application/AbschnittService.java
+++ backend/src/main/java/de/hackathondd/dvblive/application/AbschnittService.java
@@ -8,17 +8,13 @@ import java.util.Map;
 
 import de.hackathondd.dvblive.domain.Abschnitt;
 import de.hackathondd.dvblive.domain.Journey;
-import de.hackathondd.dvblive.domain.inmemorydb.HaltestellenRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AbschnittService {
-    private final HaltestellenRepository haltestellenRepository;
     private final LinienService linienService;
 
-    public AbschnittService(HaltestellenRepository haltestellenRepository,
-            LinienService linienService) {
-        this.haltestellenRepository = haltestellenRepository;
+    public AbschnittService(LinienService linienService) {
         this.linienService = linienService;
     }
 
@@ -53,6 +49,36 @@ public class AbschnittService {
             }
         }
         return abschnitte.values();
+    }
 
+    public Collection<Abschnitt> jeLinie(String triasNummer) {
+        Map<String, Abschnitt> abschnitte = new HashMap<>();
+        LinieTo linie = linienService.getLinie(triasNummer);
+        List<HaltestelleTo> haltestelleTos = linie.getHaltestellen();
+        for (int i = 0; i < linie.getHaltestellen().size() - 1; i++) {
+            HaltestelleTo current = haltestelleTos.get(i);
+            HaltestelleTo next = haltestelleTos.get(i + 1);
+            Duration maxVerspaetung = next.getJourneys().stream().map(Journey::getDifference)
+                    .max(Duration::compareTo).orElseGet(() -> Duration.ZERO);
+            Abschnitt abschnitt = new Abschnitt(current.getTriasCode(), next.getTriasCode(), maxVerspaetung,
+                    new Abschnitt.Position(current.getLatitude(), current.getLongitude()),
+                    new Abschnitt.Position(next.getLatitude(), next.getLongitude()));
+            Abschnitt existierender = abschnitte.get(current.getTriasCode() + next.getTriasCode());
+
+            if (existierender != null) {
+                existierender.addLinie(linie.getNummer());
+                if (abschnitt.getMaxVerspaetung().minus(existierender.getMaxVerspaetung()).isNegative()) {
+                    //der schon vorhandene ist größer
+                } else {
+                    //der schon vorhandene ist kleiner, also den neuen nehmen
+                    abschnitt.addLinien(existierender.getLinien());
+                    abschnitte.put(current.getTriasCode() + next.getTriasCode(), abschnitt);
+                }
+            } else {
+                abschnitt.addLinie(linie.getNummer());
+                abschnitte.put(current.getTriasCode() + next.getTriasCode(), abschnitt);
+            }
+        }
+        return abschnitte.values();
     }
 }
