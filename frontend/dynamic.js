ff2b293939f5038961ac7c2cb762a186d2e87fa6
Fabian Wunsch <fabian@uriah.heep.sax.de>
2019-11-10 03:07:17 +0100

    Haltestellen Funktionieren

--- frontend/dynamic.js
+++ frontend/dynamic.js
@@ -1,7 +1,5 @@
 var circles = [];
-async
-
-function getJsonAsync(name)
+async function getJsonAsync(name)
 {
 	let response = await fetch(`${name}`);
 	let data = await response.json()
@@ -10,11 +8,10 @@ function getJsonAsync(name)
 getJsonAsync('/dummy12.json')
 	.then(function(data) {
 	for (i = 0; i < data.length; i++){
-        circles[i] = [];
 		var tramline = data[i];
 		for (j = 0; j < tramline.haltestellen.length; j++){
 			var aktuelle_haltestelle = tramline.haltestellen[j]
-            circles[i][j] = L.circle([aktuelle_haltestelle.longitude, aktuelle_haltestelle.latitude], {
+            L.circle([aktuelle_haltestelle.longitude, aktuelle_haltestelle.latitude], {
                 color: 'blue',
                 fillColor: '#00c1ff',
                 fillOpacity: 0.2,
@@ -24,6 +21,4 @@ getJsonAsync('/dummy12.json')
 	}
 	return data;
 }).then(function(output) {
-	console.log('Berechnung fertig')
-	console.log(circles)
 });
