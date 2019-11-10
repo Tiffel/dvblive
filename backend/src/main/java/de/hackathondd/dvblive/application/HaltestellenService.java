1177e0b8f03149096d0de5cc666a87865a57827d
Christian Scheer <Christian.Scheer@t-systems.com>
2019-11-10 11:33:00 +0100

    Scheduled init und haltestellendaten

--- backend/src/main/java/de/hackathondd/dvblive/application/HaltestellenService.java
+++ backend/src/main/java/de/hackathondd/dvblive/application/HaltestellenService.java
@@ -4,10 +4,14 @@ import java.util.Set;
 
 import de.hackathondd.dvblive.domain.Haltestelle;
 import de.hackathondd.dvblive.domain.inmemorydb.HaltestellenRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HaltestellenService {
+    Logger logger = LoggerFactory.getLogger(HaltestellenService.class);
     private final VvoQueryService vvoQueryService;
     private final HaltestellenRepository haltestellenRepository;
 
@@ -17,10 +21,13 @@ public class HaltestellenService {
         this.haltestellenRepository = haltestellenRepository;
     }
 
+    @Scheduled(fixedDelay = 30000, initialDelay = 40000)
     public void updateAll() throws Exception {
+        logger.info("update haltestellen");
         Set<Haltestelle> haltestellen = haltestellenRepository.getAll();
         for (Haltestelle haltestelle : haltestellen) {
             vvoQueryService.updateJourneys(haltestelle);
         }
+        logger.info("done update haltestellen");
     }
 }
